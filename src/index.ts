28020dbf1ce537570aca138367116c8e317e9a2b
Link Li <nintendo.link@foxmail.com>
2024-12-28 16:52:02 +1100

    Unions & Enum

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,22 @@ employee = [
   [2, "John"],
   [3, "Jill"],
 ];
+
+// Union
+let pid: string | number;
+pid = "22";
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
