055b81dbd86619ace645a4633eb3e3564bebf2d4
Link Li <nintendo.link@foxmail.com>
2024-12-28 16:46:38 +1100

    Arrays & Tuples

--- dist/index.js
+++ dist/index.js
@@ -6,3 +6,12 @@ let isPublished = true;
 let x = "Hello";
 let ids = [1, 2, 3, 4, 5];
 let arr = [1, true, "Hello"];
+// Tuple
+let person = [1, "Brad", true];
+// Tuple Array
+let employee;
+employee = [
+    [1, "Brad"],
+    [2, "John"],
+    [3, "Jill"],
+];
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,14 @@ let x: any = "Hello";
 
 let ids: number[] = [1, 2, 3, 4, 5];
 let arr: any[] = [1, true, "Hello"];
+
+// Tuple
+let person: [number, string, boolean] = [1, "Brad", true];
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, "Brad"],
+  [2, "John"],
+  [3, "Jill"],
+];
